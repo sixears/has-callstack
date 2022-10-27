72d8ac7a48685fe44a825a6e85c82fa927a910b0
Mr. Fluffy <git@sixearsm>
2022-10-27 08:27:08 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -3,13 +3,15 @@
 
   inputs = {
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url  = "github:sixears/flake-build-utils/r0.0.0.0";
-    more-unicode.url = "github:sixears/more-unicode/r0.0.17.0";
-    natural.url      = "github:sixears/natural/r0.0.1.0";
+    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.0";
+
+    more-unicode.url = "github:sixears/more-unicode/r0.0.17.1";
+    natural.url      = "github:sixears/natural/r0.0.1.1";
   };
 
   outputs = { self, nixpkgs, flake-utils, build-utils, more-unicode, natural }:
     build-utils.lib.hOutputs self nixpkgs "has-callstack" {
-      inherit more-unicode natural;
+      deps = { inherit more-unicode natural; };
+      ghc  = p: p.ghc8107; # for tfmt
     };
 }
