ac4b0111da5f28f38e5313b606292461ca413a07
Mr. Fluffy <git@sixearsm>
2022-11-03 16:56:23 +0000

    gah

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.6";
 
-    more-unicode.url = "github:sixears/more-unicode/r0.0.17.5";
-    natural.url      = "github:sixears/natural/r0.0.1.5";
+    more-unicode.url = "github:sixears/more-unicode/r0.0.17.6";
+    natural.url      = "github:sixears/natural/r0.0.1.6";
   };
 
   outputs = { self, nixpkgs, build-utils, more-unicode, natural }:
