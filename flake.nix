8d39cd7953b2d28a1d828627674ee614a970fa90
Mr. Fluffy <git@sixearsm>
2022-11-19 06:33:06 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url  = github:sixears/flake-build-utils/r1.0.0.13;
 
-    more-unicode.url = github:sixears/more-unicode/r0.0.17.10;
-    natural.url      = github:sixears/natural/r0.0.1.11;
+    more-unicode.url = github:sixears/more-unicode/r0.0.17.11;
+    natural.url      = github:sixears/natural/r0.0.1.12;
   };
 
 
@@ -25,7 +25,7 @@
         in
           mkDerivation {
             pname = "has-callstack";
-            version = "1.0.1.12";
+            version = "1.0.1.16";
             src = ./.;
             libraryHaskellDepends = [
               base base-unicode-symbols lens safe strings text
